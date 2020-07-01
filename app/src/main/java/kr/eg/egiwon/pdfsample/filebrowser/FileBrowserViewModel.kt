9eec0381eced2897d2f6eb3d560383ee9cfeea64
middlefgr <2giwon@gmail.com>
2020-07-02 00:23:27 +0900

    #03 기본 구성 적용

--- app/src/main/java/kr/eg/egiwon/pdfsample/filebrowser/FileBrowserViewModel.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/filebrowser/FileBrowserViewModel.kt
@@ -1,5 +1,12 @@
 package kr.eg.egiwon.pdfsample.filebrowser
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.egiwon.scopedstorageexample.base.BaseViewModel
 
-class FileBrowserViewModel : BaseViewModel()
+class FileBrowserViewModel : BaseViewModel() {
+
+    private val _loadingBar = MutableLiveData<Boolean>()
+    val loadingBar: LiveData<Boolean> get() = _loadingBar
+
+}
--- app/src/main/java/kr/eg/egiwon/pdfsample/filebrowser/MainActivity.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/filebrowser/MainActivity.kt
@@ -1,13 +1,39 @@
 package kr.eg.egiwon.pdfsample.filebrowser
 
+import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.egiwon.scopedstorageexample.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
 import kr.eg.egiwon.pdfsample.R
 import kr.eg.egiwon.pdfsample.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity :
     BaseActivity<ActivityMainBinding, FileBrowserViewModel>(R.layout.activity_main) {
 
     override val viewModel: FileBrowserViewModel by viewModels()
+
+    private val behaviorSubject = BehaviorSubject.createDefault(0L)
+
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bind {
+            vm = viewModel
+            initAdapter()
+        }
+    }
+
+    private fun ActivityMainBinding.initAdapter() {
+        rvFiles.adapter = FileViewAdapter(viewModel = viewModel)
+        rvFiles.setHasFixedSize(true)
+        rvFiles.addItemDecoration(
+            DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL)
+        )
+    }
 
 }
