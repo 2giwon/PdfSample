2e4e80df0fcd95b8e982af9f6e2c95882640838a
middlefgr <2giwon@gmail.com>
2020-07-03 00:23:55 +0900

    #05 리싸이클러 뷰 데이터 바인딩으로 처리

---
+++ app/src/main/java/kr/eg/egiwon/pdfsample/ext/RecyclerViewExt.kt
@@ -0,0 +1,15 @@
+package kr.eg.egiwon.pdfsample.ext
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.egiwon.scopedstorageexample.base.BaseAdapter
+
+
+@Suppress("UNCHECKED_CAST")
+@BindingAdapter("replaceItems")
+fun RecyclerView.replaceItems(items: List<Any>?) {
+    (adapter as? BaseAdapter<Any, *>)?.run {
+        replaceItems(items)
+        notifyDataSetChanged()
+    }
+}
