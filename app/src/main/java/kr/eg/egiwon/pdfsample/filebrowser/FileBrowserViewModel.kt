f63a16345b776105c6546f5d0ef7a1bf6d29fb88
middlefgr <2giwon@gmail.com>
2020-07-03 00:25:46 +0900

    #08 뷰모델 데이터 처리 및 비지니스 로직 분리

--- app/src/main/java/kr/eg/egiwon/pdfsample/filebrowser/FileBrowserViewModel.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/filebrowser/FileBrowserViewModel.kt
@@ -1,12 +1,130 @@
 package kr.eg.egiwon.pdfsample.filebrowser
 
+import android.net.Uri
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.egiwon.scopedstorageexample.base.BaseViewModel
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import kr.eg.egiwon.pdfsample.Event
+import kr.eg.egiwon.pdfsample.data.FileBrowserRepository
+import kr.eg.egiwon.pdfsample.filebrowser.model.DocumentItem
+import kr.eg.egiwon.pdfsample.util.DocumentProvider
+import java.util.*
 
-class FileBrowserViewModel : BaseViewModel() {
+class FileBrowserViewModel @ViewModelInject constructor(
+    private val repository: FileBrowserRepository,
+    private val documentProvider: DocumentProvider
+) : BaseViewModel() {
+
+    private val _directoryUri = MutableLiveData<Event<Uri>>()
+    val directoryUri: LiveData<Event<Uri>> get() = _directoryUri
+
+    private val _documents = MutableLiveData<List<DocumentItem>>()
+    val documents: LiveData<List<DocumentItem>> get() = _documents
 
     private val _loadingBar = MutableLiveData<Boolean>()
     val loadingBar: LiveData<Boolean> get() = _loadingBar
+
+    private val _directoryLiveData = MutableLiveData<Event<DocumentItem>>()
+    val directoryLiveData: LiveData<Event<DocumentItem>> get() = _directoryLiveData
+
+    private val _documentLiveData = MutableLiveData<Event<DocumentItem>>()
+    val documentLiveData: LiveData<Event<DocumentItem>> get() = _documentLiveData
+
+    private val documentList = mutableListOf<DocumentItem>()
+
+    private val breadCrumbsStack = Stack<Uri>()
+
+    private val _isBackBreadCrumbsEmpty = MutableLiveData<Event<Boolean>>()
+    val isBackBreadCrumbsEmpty: LiveData<Event<Boolean>> get() = _isBackBreadCrumbsEmpty
+
+    override fun onClick(model: Any?) {
+        (model as? DocumentItem)?.let { document ->
+            if (document.isDirectory) {
+                _directoryLiveData.postValue(Event(document))
+                addBreadCrumbs(_directoryUri.value?.peekContent() ?: return)
+            } else {
+                _documentLiveData.postValue(Event(document))
+            }
+        }
+    }
+
+    fun setDirectoryUri(directoryUri: Uri) {
+        _directoryUri.value = Event(directoryUri)
+    }
+
+    fun loadDirectory(documentUri: Uri) {
+        Observable.create<List<DocumentItem>> { emit ->
+            documentList.clear()
+            runCatching {
+                val resultList = documentProvider.getDocuments(documentUri)
+                documentList.addAll(resultList)
+            }
+                .onSuccess { emit.onComplete() }
+                .onFailure { emit.onError(it) }
+        }
+            .doOnSubscribe { _loadingBar.postValue(true) }
+            .doAfterTerminate { _loadingBar.postValue(false) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onError = {
+                    errorThrowableMutableLiveData.value = it
+                },
+                onComplete = {
+                    _documents.value = documentList
+                        .asSequence()
+                        .filter { !(it.name.startsWith(".")) }
+                        .sortedWith(compareBy({ !it.isDirectory }, {
+                            it.name.toLowerCase(Locale.getDefault())
+                        }))
+                        .toList()
+                }
+            )
+            .addTo(compositeDisposable)
+    }
+
+    fun setDocumentUri(uri: Uri) {
+        _directoryUri.value = Event(uri)
+    }
+
+    fun saveRootUri(uri: String) {
+        Completable.create {
+            repository.saveRootUri(uri)
+            it.onComplete()
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+            .addTo(compositeDisposable)
+    }
+
+    fun loadRootUri() {
+        repository.loadRootUri()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy {
+                setDocumentUri(Uri.parse(it))
+            }
+            .addTo(compositeDisposable)
+    }
+
+    private fun addBreadCrumbs(uri: Uri) {
+        breadCrumbsStack.push(uri)
+    }
+
+    fun backBreadCrumbs() {
+        if (breadCrumbsStack.isNotEmpty()) {
+            setDocumentUri(breadCrumbsStack.pop())
+        } else {
+            _isBackBreadCrumbsEmpty.value = Event(true)
+        }
+    }
 
 }
--- app/src/main/java/kr/eg/egiwon/pdfsample/filebrowser/MainActivity.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/filebrowser/MainActivity.kt
@@ -1,14 +1,19 @@
 package kr.eg.egiwon.pdfsample.filebrowser
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.egiwon.scopedstorageexample.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.subjects.BehaviorSubject
+import kr.eg.egiwon.pdfsample.EventObserver
 import kr.eg.egiwon.pdfsample.R
 import kr.eg.egiwon.pdfsample.databinding.ActivityMainBinding
+import kr.eg.egiwon.pdfsample.filebrowser.model.DocumentItem
 
 @AndroidEntryPoint
 class MainActivity :
@@ -26,6 +31,33 @@ class MainActivity :
             vm = viewModel
             initAdapter()
         }
+
+        viewModel.loadRootUri()
+        addObserve()
+
+        processBackPressedExit()
+    }
+
+    override fun addObserve() {
+        viewModel.directoryUri.observe(this, EventObserver {
+            if (it.toString().isNotEmpty()) {
+                viewModel.loadDirectory(it)
+            } else {
+                openIntentDirectory()
+            }
+        })
+
+        viewModel.directoryLiveData.observe(this, EventObserver { document ->
+            viewModel.setDocumentUri(document.uri)
+        })
+
+        viewModel.documentLiveData.observe(this, EventObserver { document ->
+            openDocument(document)
+        })
+
+        viewModel.isBackBreadCrumbsEmpty.observe(this, EventObserver {
+            behaviorSubject.onNext(System.currentTimeMillis())
+        })
     }
 
     private fun ActivityMainBinding.initAdapter() {
@@ -34,6 +66,72 @@ class MainActivity :
         rvFiles.addItemDecoration(
             DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL)
         )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (OPEN_DIRECTORY_REQUEST_CODE == requestCode) {
+            if (resultCode == RESULT_OK) {
+                val directoryUri = data?.data ?: return
+
+                contentResolver.takePersistableUriPermission(
+                    directoryUri,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                )
+
+                viewModel.setDirectoryUri(directoryUri)
+                viewModel.saveRootUri(directoryUri.toString())
+            } else {
+                finish()
+            }
+
+        }
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        super.onDestroy()
+    }
+
+    private fun openIntentDirectory() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or
+                    Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
+        }
+
+        startActivityForResult(intent, OPEN_DIRECTORY_REQUEST_CODE)
+    }
+
+    override fun onBackPressed() {
+        viewModel.backBreadCrumbs()
+    }
+
+    private fun processBackPressedExit() {
+        behaviorSubject.buffer(2, 1)
+            .map { it[0] to it[1] }
+            .subscribe {
+                if (it.second - it.first < 2000L) {
+                    super.onBackPressed()
+                } else {
+                    showToast(R.string.back_press_exit)
+                }
+            }.addTo(compositeDisposable)
+    }
+
+    private fun openDocument(item: DocumentItem) {
+        try {
+            val openIntent = Intent(Intent.ACTION_VIEW).apply {
+                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                data = item.uri
+            }
+            startActivity(openIntent)
+        } catch (ex: ActivityNotFoundException) {
+            showToast(getString(R.string.error_invalid_activity, item.name))
+        }
+    }
+
+    companion object {
+        private const val OPEN_DIRECTORY_REQUEST_CODE = 0xf11e
     }
 
 }
