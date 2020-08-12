b1fa9d5763a42150f359156ccccc29346b9e9fd6
middlefgr <2giwon@gmail.com>
2020-08-13 00:06:29 +0900

    #22 페이지 adpter 추가

--- app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/PdfViewActivity.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/PdfViewActivity.kt
@@ -4,10 +4,13 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import kr.eg.egiwon.pdfsample.BR
 import kr.eg.egiwon.pdfsample.R
 import kr.eg.egiwon.pdfsample.base.BaseActivity
+import kr.eg.egiwon.pdfsample.base.BaseAdapter2
 import kr.eg.egiwon.pdfsample.databinding.ActivityPdfBinding
 import kr.eg.egiwon.pdfsample.filebrowser.MainActivity.Companion.OPEN_DOCUMENT_URI
+import kr.eg.egiwon.pdfsample.pdfview.model.PdfPage
 
 @AndroidEntryPoint
 class PdfViewActivity : BaseActivity<ActivityPdfBinding, PdfViewModel>(
@@ -18,6 +21,16 @@ class PdfViewActivity : BaseActivity<ActivityPdfBinding, PdfViewModel>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        bind {
+            vm = viewModel
+            rvPdf.adapter = object : BaseAdapter2(
+                BR.pdfPage,
+                mapOf(BR.vm to viewModel),
+                mapOf(PdfPage::class to R.layout.item_pdf_page)
+
+            ) {}
+            rvPdf.setHasFixedSize(true)
+        }
         intent.extras?.getString(OPEN_DOCUMENT_URI)?.let {
             val uri = Uri.parse(it)
             runCatching {
