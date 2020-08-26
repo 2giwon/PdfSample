b17650a8ff3d18b504d503fc2f3442d93501279b
middlefgr <2giwon@gmail.com>
2020-08-26 23:51:03 +0900

    #38 리싸이클러뷰 scale 되도록 작업

--- app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/PdfRecyclerView.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/PdfRecyclerView.kt
@@ -3,33 +3,68 @@ package kr.eg.egiwon.pdfsample.pdfview
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import androidx.recyclerview.widget.RecyclerView
+import kr.eg.egiwon.pdfsample.pdfview.internal.ActionListener
+import kr.eg.egiwon.pdfsample.pdfview.internal.ScaleAnimator
+import kr.eg.egiwon.pdfsample.pdfview.internal.ScaleAnimatorImpl
 
 class PdfRecyclerView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : RecyclerView(context, attrs, defStyleAttr) {
+) : RecyclerView(context, attrs, defStyleAttr), ActionListener {
 
     private var isScaled = false
+
+    private val scaleAnimator: ScaleAnimator = ScaleAnimatorImpl(
+        this, DEFAULT_MAX_SCALE
+    )
+
+    private val scaleListener =
+        object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+            override fun onScaleEnd(detector: ScaleGestureDetector) {
+                super.onScaleEnd(detector)
+                this@PdfRecyclerView.onScaleEnded()
+            }
+
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                this@PdfRecyclerView.onScaled(detector.scaleFactor)
+                return true
+            }
+        }
+
+    private val scaleGestureDetector = ScaleGestureDetector(context, scaleListener)
 
     @Suppress("ClickableViewAccessibility")
     override fun onTouchEvent(e: MotionEvent): Boolean {
 //        return false
-        return when (e.actionMasked and MotionEvent.ACTION_MASK) {
-            MotionEvent.ACTION_POINTER_DOWN,
-            MotionEvent.ACTION_POINTER_UP -> {
-                isScaled = true
-                !isScaled
-            }
-            MotionEvent.ACTION_DOWN -> super.onTouchEvent(e)
-            MotionEvent.ACTION_UP -> {
-                val ret = if (isScaled) false else super.onTouchEvent(e)
-                isScaled = false
-                ret
-            }
-            MotionEvent.ACTION_MOVE -> if (isScaled) false else super.onTouchEvent(e)
-            else -> super.onTouchEvent(e)
-        }
+        scaleGestureDetector.onTouchEvent(e)
+        return super.onTouchEvent(e)
+    }
+
+    override fun onScaled(scale: Float) {
+        scaleAnimator.scale(scale)
+    }
+
+    override fun onScaleEnded() {
+        scaleAnimator.adjust()
+    }
+
+    override fun onMoved(dx: Float, dy: Float) {
+
+    }
+
+    override fun onFling(velocityX: Float, velocityY: Float) {
+
+    }
+
+    override fun onMoveEnded() {
+
+    }
+
+    companion object {
+        private const val DEFAULT_MAX_SCALE = 25f
     }
 }
