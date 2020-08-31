2b3cbfc0dc5ecdc0feb68e6e7342f1f5351d4b21
2giwon <2giwon@gmail.com>
2020-08-31 18:37:38 +0900

    #45 문서 로딩 작업 1단계 중간 커밋

--- app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/PdfPageView.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/PdfPageView.kt
@@ -1,28 +1,34 @@
 package kr.eg.egiwon.pdfsample.pdfview
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.RectF
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.widget.FrameLayout
+import kr.eg.egiwon.pdfsample.pdfview.internal.ActionListener
+import kr.eg.egiwon.pdfsample.pdfview.internal.ScaleAnimator
+import kr.eg.egiwon.pdfsample.pdfview.internal.ScaleAnimatorImpl
 import kr.eg.egiwon.pdfsample.pdfview.model.PdfPage
+import kotlin.math.max
+import kotlin.math.min
 
 class PdfPageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr), ActionListener {
 
-    private var frame: RectF? = null
+    private var scaleFactor = 1.0f
 
-    private val paint = Paint().apply {
-        isAntiAlias = true
-    }
+    private val scaleAnimator: ScaleAnimator = ScaleAnimatorImpl(this, DEFAULT_MAX_SCALE)
 
-    private val pageBitmaps: MutableList<Bitmap> = mutableListOf()
+    private val paint = Paint()
+
+    private val pdfPages: MutableList<PdfPage> = mutableListOf()
     private var viewHeight = 0f
+    private var pageCount = 0
 
     init {
         setWillNotDraw(false)
@@ -30,14 +36,6 @@ class PdfPageView @JvmOverloads constructor(
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-
-//        val height = measuredHeight.toFloat()
-//        val width = measuredWidth.toFloat()
-//        val frameRect = this@PdfPageView.frame
-//        val widthScale = if (frameRect != null) frameRect.width() / width else 1f
-//        val heightScale = if (frameRect != null) frameRect.height() / height else 1f
-//        val scale = maxOf(widthScale, heightScale)
-//        setMeasuredDimension((width * scale).toInt(), (height * scale).toInt())
 
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
         val heightSize = MeasureSpec.getSize(heightMeasureSpec)
@@ -54,29 +52,94 @@ class PdfPageView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
-        pageBitmaps.forEachIndexed { index, bitmap ->
+        pdfPages.forEach { pdfPage ->
             canvas?.drawBitmap(
-                bitmap,
-                ((measuredWidth - bitmap.width) / 2).toFloat(),
-                ((bitmap.height + PAGE_DIVIDER) * index).toFloat(),
+                pdfPage.pageBitmap,
+                ((measuredWidth - pdfPage.pageBitmap.width) / 2).toFloat(),
+                ((pdfPage.pageBitmap.height + PAGE_DIVIDER) * pdfPage.pageNum).toFloat(),
                 paint
             )
         }
-
-    }
-
-    fun setFrame(frame: RectF) {
-        this.frame = frame
     }
 
     fun addPdfPage(page: PdfPage) {
-        pageBitmaps += page.pageBitmap
-        viewHeight += page.pageBitmap.height + PAGE_DIVIDER
-        requestLayout()
+        pdfPages.add(page)
+//        drawContents()
+        postInvalidate()
+    }
+
+//    private fun drawContents() {
+//        synchronized(surfaceHolder) {
+//            var canvas: Canvas? = null
+//            try {
+//                canvas = surfaceHolder.lockCanvas()
+//                draw(canvas)
+//
+//            } finally {
+//                if (canvas != null) {
+//                    surfaceHolder.unlockCanvasAndPost(canvas)
+//                }
+//            }
+//        }
+//    }
+
+    fun setPageCount(pageCount: Int) {
+        this.pageCount = pageCount
+    }
+
+    fun setPageHeight(height: Int) {
+        if (viewHeight <= 0 && pageCount > 0) {
+            viewHeight = (height + PAGE_DIVIDER).toFloat() * pageCount
+            requestLayout()
+        }
+    }
+
+    private val scaleListener =
+        object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+            override fun onScaleEnd(detector: ScaleGestureDetector) {
+                super.onScaleEnd(detector)
+                this@PdfPageView.onScaleEnded()
+            }
+
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                scaleFactor *= detector.scaleFactor
+                scaleFactor = max(0.1f, min(scaleFactor, 5.0f))
+                this@PdfPageView.onScaled(scaleFactor)
+                return true
+            }
+        }
+
+    private val scaleGestureDetector = ScaleGestureDetector(context, scaleListener)
+
+    @Suppress("ClickableViewAccessibility")
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+
+        return scaleGestureDetector.onTouchEvent(e)
+    }
+
+    override fun onScaled(scale: Float) {
+        scaleAnimator.scale(scale)
+    }
+
+    override fun onScaleEnded() {
+        scaleAnimator.adjust()
+    }
+
+    override fun onMoved(dx: Float, dy: Float) {
+
+    }
+
+    override fun onFling(velocityX: Float, velocityY: Float) {
+
+    }
+
+    override fun onMoveEnded() {
+
     }
 
     companion object {
+        private const val DEFAULT_MAX_SCALE = 25f
         private const val PAGE_DIVIDER = 10
     }
 }
--- app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/model/PdfPage.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/model/PdfPage.kt
@@ -6,7 +6,9 @@ import kr.eg.egiwon.pdfsample.base.BaseIdentifier
 data class PdfPage(
     val identifier: Int,
     val pageBitmap: Bitmap,
-    val pageNum: Int
+    val pageNum: Int,
+    val width: Int,
+    val height: Int
 ) : BaseIdentifier() {
     override val id: Any
         get() = identifier
