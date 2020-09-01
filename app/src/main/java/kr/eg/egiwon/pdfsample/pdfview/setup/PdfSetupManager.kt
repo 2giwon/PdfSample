125d2b2f8370b9367481d830778676c666b61f41
2giwon <2giwon@infrawareglobal.com>
2020-09-01 16:20:37 +0900

    Revert "#51 page setup 클래스 추가 (page size, page count 처리)"
    This reverts commit c66852d0

--- app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/setup/PdfSetupManager.kt
+++
@@ -1,9 +0,0 @@
-package kr.eg.egiwon.pdfsample.pdfview.setup
-
-import kr.eg.egiwon.pdfsample.util.Size
-
-interface PdfSetupManager {
-
-    fun pageSetup(viewSize: Size<Int>, getPageCount: (Int) -> Unit, setupComplete: () -> Unit)
-
-}
