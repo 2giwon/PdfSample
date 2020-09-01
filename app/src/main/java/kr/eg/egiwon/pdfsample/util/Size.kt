c56f69e1e25294231492992cc25dfb5bbc5c6b71
2giwon <2giwon@gmail.com>
2020-09-01 16:31:50 +0900

    #47 Size 클래스 제네릭 적용 (Float 타입도 사용가능하게)

--- app/src/main/java/kr/eg/egiwon/pdfsample/util/Size.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/util/Size.kt
@@ -1,8 +1,8 @@
 package kr.eg.egiwon.pdfsample.util
 
-class Size(
-    val width: Int,
-    val height: Int
+class Size<T : Number>(
+    val width: T,
+    val height: T
 ) {
 
     override fun equals(other: Any?): Boolean {
@@ -14,16 +14,21 @@ class Size(
             return true
         }
 
-        if (other is Size) {
-            val obj = other
-            return width == obj.width && height == obj.height
+        if (other is Size<*>) {
+            return width == other.width && height == other.height
         }
 
         return false
     }
 
     override fun hashCode(): Int {
-        return height xor (width shl Integer.SIZE / 2 or (width ushr Integer.SIZE / 2))
+        if (height is Int && width is Int) {
+            return height xor (width shl Integer.SIZE / 2 or (width ushr Integer.SIZE / 2))
+        } else if (height is Float && width is Float) {
+            return width.toBits() xor height.toBits()
+        }
+
+        return super.hashCode()
     }
 
     override fun toString(): String {
