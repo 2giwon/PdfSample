2c5638d5c36951a197b39047609013fdac28f0e9
2giwon <2giwon@gmail.com>
2020-09-07 19:04:27 +0900

    #57 pagepart, rendertask 추가

---
+++ app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/render/model/PagePart.kt
@@ -0,0 +1,11 @@
+package kr.eg.egiwon.pdfsample.pdfview.render.model
+
+import android.graphics.Bitmap
+import android.graphics.RectF
+
+data class PagePart(
+    val page: Int,
+    val renderedPart: Bitmap,
+    val bounds: RectF,
+    val cacheOrder: Int
+)
---
+++ app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/render/model/RenderTask.kt
@@ -0,0 +1,13 @@
+package kr.eg.egiwon.pdfsample.pdfview.render.model
+
+import android.graphics.RectF
+
+data class RenderTask(
+    val width: Float,
+    val height: Float,
+    val bounds: RectF,
+    val page: Int,
+    val thumbnail: Boolean = false,
+    val cacheOrder: Int,
+    val isAnnotationRendering: Boolean
+)
