60a36b3fbdc2ddb51f46c6c8334b78f0ee77b470
2giwon <2giwon@gmail.com>
2020-09-08 15:07:37 +0900

    #64 코드 네이밍 변경 직접 렌더링을 담당하는 것은 아니라서 렌더 task를 생성해주는 manager이므로 변경

--- app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/render/RenderTaskManager.kt
+++ app/src/main/java/kr/eg/egiwon/pdfsample/pdfview/render/RenderTaskManager.kt
@@ -4,8 +4,8 @@ import android.graphics.RectF
 import kr.eg.egiwon.pdfsample.pdfview.render.model.PagePart
 import kr.eg.egiwon.pdfsample.pdfview.render.model.RenderTask
 
-interface RenderManager {
-    fun render(task: RenderTask): PagePart
+interface RenderTaskManager {
+    fun makePagePart(task: RenderTask): PagePart?
 
     fun createRenderTask(
         page: Int,
